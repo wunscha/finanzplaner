3180b35cfd463b2c88a3ec250531c5d1bb9d8ac5
wunscha <richhard.wansch@gmail.com>
2023-03-26 12:35:11 +0200

    diagrammdaten zeitpunkt

--- src/components/analysenZeitpunkt/ItemAnalyseZeitpunkt.vue
+++ src/components/analysenZeitpunkt/ItemAnalyseZeitpunkt.vue
@@ -19,22 +19,39 @@
     </q-card-section>
 
     <q-card-section>
-      <apexchart
+      <div class="text-bold">
+        options:
+      </div>
+      <div>
+        {{ diagramm.options }}
+      </div>
+
+      <div class="text-bold">
+        series:
+      </div>
+      <div>
+        {{ diagramm.series }}
+      </div>
+
+      <div class="text-bold">
+        categories:
+      </div>
+      <div>
+        {{ diagramm.categories }}
+      </div>
+      <!-- <apexchart
         v-if="diagrammdaten.options.xaxis.categories.length > 0"
         type="bar"
         height="350"
         :options="diagrammdaten.options"
         :series="diagrammdaten.series"
-      ></apexchart>
+      ></apexchart> -->
     </q-card-section>
   </q-card>
 </template>
 
 <script>
   import { defineComponent, ref } from 'vue'
-  import datastore from 'src/_Data/datastore';
-  import { ENUM_BUCHUNGSINTERVALLE } from 'src/_Domain/models';
-  import datastoremanager from 'src/_DataManipulation/datastoremanager';
   import style from 'src/_Data/style';
   import VueApexChart from 'vue3-apexcharts';
 
@@ -48,122 +65,36 @@
     props: [
       'szenario',
       'kontenAktuell',
-      'datumAktuell',
+      'kontostaende',
+      'kontostandMax',
+      'kontostandMin',
     ],
     data() {
       return {
         style: style,
-        datastore: datastore,
-        ladezustand: ref(false),
-        yaxisMax: ref(0),
-        yaxisMin: ref(0),
       }
     },
     computed: {
-      datum() {return new Date(this.datumAktuell);},
-      diagrammdaten() {
-        let datum = new Date(this.datumAktuell);
-        this.ladezustand = true;
-        // filtere Buchungsreihen
-        let idsKontenAktuell = this.kontenAktuell.map(k => k.id);
-        let buchungsreihenGefiltert = this.datastore.buchungsreihen.filter(br =>
-          (br.szenario.id == this.szenario.id || br.szenario.istAllgemein) &&
-          (
-            idsKontenAktuell.includes(br.quellkonto.id) ||
-            idsKontenAktuell.includes(br.zielkonto.id)
-          )
-        );
-        // initialisiere kontrollparameterBuchungsreihen
-        let kontrollparameterBuchungsreihen = {};
-        for (let buchungsreihe of buchungsreihenGefiltert) {
-          let kontrollparameter = {
-            datumNaechsteBuchung: new Date(buchungsreihe.datumAnfang),
-          };
-          kontrollparameterBuchungsreihen[buchungsreihe.id] = kontrollparameter;
-        };
-        // initialisiere kontostaende
-        let kontostaende = {};
+      /**
+       * Alle Informationen, die für rendern der Diagramm erforderlich sind werden zusammengefasst und zurückgegeben,
+       * damit keine Probleme auftreten, weil manche Werte früher zur Verfügung stehen als andere
+       */
+      diagramm() {
+        let categories = [];
+        let series = [{data: []}];
         for (let konto of this.kontenAktuell) {
-          kontostaende[konto.id] = konto.kontostandInitial;
+          categories.push(konto.bezeichnung);
+          let kontostand = this.kontostaende[konto.id];
+          series[0].data.push(kontostand);
         }
-        // berechne Kontostaende
-        for (let buchungsreihe of buchungsreihenGefiltert) {
-          let kontrollparameter = kontrollparameterBuchungsreihen[buchungsreihe.id];
-          let datumEndeBuchungsreihe = new Date(buchungsreihe.datumEnde);
-          while (!kontrollparameter.istAbgeschlossen) {
-            kontrollparameter.istAbgeschlossen =
-              kontrollparameter.istAbgeschlossen ||
-              kontrollparameter.datumNaechsteBuchung > datum ||
-              kontrollparameter.datumNaechsteBuchung > datumEndeBuchungsreihe;
-            if (kontrollparameter.istAbgeschlossen) { continue; }
-            // Kontoausgang
-            if (kontostaende[buchungsreihe.quellkonto.id] != undefined) {
-              kontostaende[buchungsreihe.quellkonto.id] -= parseInt(buchungsreihe.betrag);
-            }
-            // Kontoeingang
-            if (kontostaende[buchungsreihe.zielkonto.id] != undefined) {
-              kontostaende[buchungsreihe.zielkonto.id] += parseInt(buchungsreihe.betrag);
-            }
-            // berechne datumNaehsteBuchung
-            switch (buchungsreihe.buchungsintervall) {
-              case ENUM_BUCHUNGSINTERVALLE.woechentlich:
-                let tagAktuell = kontrollparameter.datumNaechsteBuchung.getDate();
-                kontrollparameter.datumNaechsteBuchung.setDate(tagAktuell + 7);
-                break;
-              case ENUM_BUCHUNGSINTERVALLE.monatlich:
-                let monatAktuell = kontrollparameter.datumNaechsteBuchung.getMonth();
-                kontrollparameter.datumNaechsteBuchung.setMonth(monatAktuell + 1);
-                break;
-              case ENUM_BUCHUNGSINTERVALLE.jaehrlich:
-                let jahrAktuell = kontrollparameter.datumNaechsteBuchung.getFullYear();
-                kontrollparameter.datumNaechsteBuchung.setFullYear(jahrAktuell + 1);
-                break;
-              case ENUM_BUCHUNGSINTERVALLE.einzel:
-                kontrollparameter.istAbgeschlossen = true;
-                break;
-            }
-          }
-        }
-        // erzeuge diagrammdaten für Apex-Charts
 
-
-        let optionsDiagramm = {
-          chart: {
-            animations: {enabled: false},
-            type: 'bar',
-            height: 350
-          },
-          plotOptions: {
-            bar: {
-              borderRadius: 4,
-              horizontal: true,
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          xaxis: {
-            categories: [],
-          },
-          yaxis: {
-          }
+        let options = 'nicht implementiert';
+        return {
+          series: series,
+          categories: categories,
+          options: options,
         }
-        let data = [];
-        for (let [key, val] of Object.entries(kontostaende)) {
-          let konto = datastoremanager.read(key, datastoremanager.keys.konten);
-          optionsDiagramm.xaxis.categories.push(konto.bezeichnung);
-          // TODO: Wenn Probleme mit Rendern von Chart auftreten: wenn val == 0 wird von apexcharts immer "expectedValue"-Error geworfen ==> ggf. irgendeneinen Pseudowert verwenden, wenn Kontostand = 0
-          data.push(val);
-        }
-        let diagrammdaten = {
-          options: optionsDiagramm,
-          series: [{data: data}]
-        };
-        // Ladezustand beenden
-        this.ladezustand = false;
-
-        return diagrammdaten;
-      }
+      },
     },
   });
 </script>
--- src/pages/AnalysenZeitpunkt.vue
+++ src/pages/AnalysenZeitpunkt.vue
@@ -26,12 +26,25 @@
       stack-label
     />
 
-    <div class="column items-center q-gutter-md q-pa-md">
+    <!-- Ladezustand -->
+    <div
+      v-if="ladeszustand"
+      class="row justify-center q-pa-xl"
+    >
+      <q-spinner size="5rem" />
+    </div>
+    <div
+      v-else
+      class="column items-center q-gutter-md q-pa-md"
+    >
+      <!-- {{ diagrammdaten }} -->
       <ItemAnalyseZeitpunkt
         v-for="szenario in szenarienAktuell"
         :szenario="szenario"
         :konten-aktuell="kontenAktuell"
-        :datum-aktuell="datumAktuell"
+        :kontostaende="diagrammdaten.kontostaende[szenario.id]"
+        :kontostand-min="diagrammdaten.kontostandMin"
+        :kontostand-max="diagrammdaten.kontostandMax"
       />
     </div>
   </q-page>
@@ -41,7 +54,8 @@
   import { defineComponent, ref } from 'vue'
   import ItemAnalyseZeitpunkt from 'src/components/analysenZeitpunkt/ItemAnalyseZeitpunkt.vue'
   import datastore from '../_Data/datastore'
-  import formatierer from 'src/_Application/formatierer';
+  import formatierer from 'src/_Application/formatierer'
+  import { ENUM_BUCHUNGSINTERVALLE } from 'src/_Domain/models'
 
   export default defineComponent({
     name: 'Konten',
@@ -51,10 +65,129 @@
     data() {
       return {
         datastore: datastore,
+        ladeszustand: ref(false),
         szenarienAktuell: ref([datastore.szenarien[0], ]),
         kontenAktuell: ref([]),
         datumAktuell: ref(formatierer.dateFuerInput(datastore.datumMax)),
       }
     },
+    computed: {
+      /**
+       * Alle Informationen, die für rendern der Diagramm erforderlich sind werden zusammengefasst und zurückgegeben,
+       * damit keine Probleme auftreten, weil manche Werte (zB kontenAktuell) früher zur Verfügung stehen als andere (zB kontostaende)
+       */
+      diagrammdaten() {
+
+        let datum = new Date(this.datumAktuell);
+        this.ladezustand = true;
+        // filtere Buchungsreihen
+        let idsKontenAktuell = this.kontenAktuell.map(ko => ko.id);
+        let idsSzenarienAktuell = this.szenarienAktuell.map(sz => sz.id);
+        let buchungsreihenGefiltert = this.datastore.buchungsreihen.filter(br =>
+          (
+            br.szenario.istAllgemein ||
+            idsSzenarienAktuell.includes(br.szenario.id)
+          )
+            &&
+          (
+            idsKontenAktuell.includes(br.quellkonto.id) ||
+            idsKontenAktuell.includes(br.zielkonto.id)
+          )
+        );
+        // initialisiere kontrollparameterBuchungsreihen
+        let kontrollparameterBuchungsreihen = {};
+        for (let buchungsreihe of buchungsreihenGefiltert) {
+          let kontrollparameter = {
+            datumNaechsteBuchung: new Date(buchungsreihe.datumAnfang),
+          };
+          kontrollparameterBuchungsreihen[buchungsreihe.id] = kontrollparameter;
+        };
+        // initialisiere kontostaende
+        let kontostaende = {};
+        for (let szenario of this.szenarienAktuell) {
+          kontostaende[szenario.id] = {};
+          for (let konto of this.kontenAktuell) {
+            kontostaende[szenario.id][konto.id] = konto.kontostandInitial;
+          }
+        }
+        // berechne Kontostaende
+        for (let buchungsreihe of buchungsreihenGefiltert) {
+          let kontrollparameter = kontrollparameterBuchungsreihen[buchungsreihe.id];
+          let datumEndeBuchungsreihe = new Date(buchungsreihe.datumEnde);
+          while (!kontrollparameter.istAbgeschlossen) {
+            kontrollparameter.istAbgeschlossen =
+              kontrollparameter.istAbgeschlossen ||
+              kontrollparameter.datumNaechsteBuchung > datum ||
+              kontrollparameter.datumNaechsteBuchung > datumEndeBuchungsreihe;
+            if (kontrollparameter.istAbgeschlossen) { continue; }
+            // Kontoausgang
+            if (kontostaende[buchungsreihe.szenario.id][buchungsreihe.quellkonto.id] != undefined) {
+              // wenn Szenario Allgemein -> für alle Szenarien buchen
+              if (buchungsreihe.istAllgemein) {
+                for (let idSz of idsSzenarienAktuell) {
+                  kontostaende[idSz][buchungsreihe.quellkonto.id] -= parseInt(buchungsreihe.betrag);
+                }
+              } else {
+                kontostaende[buchungsreihe.szenario.id][buchungsreihe.quellkonto.id] -= parseInt(buchungsreihe.betrag);
+              }
+            }
+            // Kontoeingang
+            if (kontostaende[buchungsreihe.szenario.id][buchungsreihe.zielkonto.id] != undefined) {
+              // wenn Szenario Allgemein -> für alle Szenarien buchen
+              if (buchungsreihe.istAllgemein) {
+                for (let idSz of idsSzenarienAktuell) {
+                  kontostaende[idSz][buchungsreihe.zielkonto.id] += parseInt(buchungsreihe.betrag);
+                }
+              } else {
+                kontostaende[buchungsreihe.szenario.id][buchungsreihe.zielkonto.id] += parseInt(buchungsreihe.betrag);
+              }
+            }
+            // berechne datumNaehsteBuchung
+            switch (buchungsreihe.buchungsintervall) {
+              case ENUM_BUCHUNGSINTERVALLE.woechentlich:
+                let tagAktuell = kontrollparameter.datumNaechsteBuchung.getDate();
+                kontrollparameter.datumNaechsteBuchung.setDate(tagAktuell + 7);
+                break;
+              case ENUM_BUCHUNGSINTERVALLE.monatlich:
+                let monatAktuell = kontrollparameter.datumNaechsteBuchung.getMonth();
+                kontrollparameter.datumNaechsteBuchung.setMonth(monatAktuell + 1);
+                break;
+              case ENUM_BUCHUNGSINTERVALLE.jaehrlich:
+                let jahrAktuell = kontrollparameter.datumNaechsteBuchung.getFullYear();
+                kontrollparameter.datumNaechsteBuchung.setFullYear(jahrAktuell + 1);
+                break;
+              case ENUM_BUCHUNGSINTERVALLE.einzel:
+                kontrollparameter.istAbgeschlossen = true;
+                break;
+            }
+          }
+        }
+
+        // zusammenfasse Diagrammdaten
+        let diagrammdaten = {
+          kontostaende: kontostaende,
+          kontostandMax: 0,
+          kontostandMin: 0,
+          kontenAktuell: this.kontenAktuell,
+        }
+
+        // ermmitle KontsotandMax und KontostandMin
+        for (let sz of Object.values(kontostaende)) {
+          for (let ks of Object.values(sz)) {
+            if (ks > diagrammdaten.kontostandMax) {
+              diagrammdaten.kontostandMax = ks;
+            }
+            if (ks < diagrammdaten.kontostandMin) {
+              diagrammdaten.kontostandMin = ks;
+            }
+          }
+        }
+
+        // Ladezustand beenden
+        this.ladezustand = false;
+
+        return diagrammdaten;
+      }
+    }
   });
 </script>
